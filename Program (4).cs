4329e557a021ecde61c9cc14533044b0a07ec752
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-11-10 20:28:34 +0200

    Add files via upload

---
+++ Program (4).cs
@@ -0,0 +1,11 @@
+﻿var n = int.Parse(Console.ReadLine());
+var x = double.Parse(Console.ReadLine());
+
+double result = 0;
+for (var i = 0; i < n; i++) {
+    var factorial = 1;
+    for (var j = 1; j < i; j++) factorial += j;
+    
+    result += Math.Cos(x * i) / factorial;
+}
+Console.WriteLine(result);
